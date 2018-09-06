aa0640fd6e8094ed8ea5c56a6672b4b93d4b1258
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-09-06 05:12:44 -0700

    drivers: nexusfb: add composition output fallback.
    if composite output is supported on the platform and if hdmi output
    cannot be opened, fallback to composite output for blit and post the
    framebuffer update.
    
    *** this driver is solely used for android recovery mode support.
    
    refs #SWANDROID-5446
    
    Change-Id: I9196cff72f17ebb13ac4ee3a67553eaeb37571d6
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- fbdev/bcmnexusfb.c
+++ fbdev/bcmnexusfb.c
@@ -26,19 +26,29 @@
 #if NEXUS_HAS_HDMI_OUTPUT
 #include "nexus_hdmi_output.h"
 #endif
+#if NEXUS_NUM_DISPLAYS > 1 && NEXUS_NUM_COMPOSITE_OUTPUTS
+#include "nexus_composite_output.h"
+#endif
 #include <linux/brcmstb/brcmstb.h>
 
 static int debug_verbose = 0;
 
+struct bcmnexusfb_token {
+	NEXUS_DisplayHandle display;
+	NEXUS_SurfaceHandle framebuffer;
+	NEXUS_SurfaceHandle fb0;
+	NEXUS_SurfaceHandle fb1;
+	NEXUS_SurfaceMemory mem;
+};
+
 struct bcmnexusfb_par {
-	  NEXUS_DisplayHandle display;
-	  NEXUS_Graphics2DHandle gfx;
-	  NEXUS_SurfaceHandle framebuffer;
-	  NEXUS_SurfaceHandle fb0;
-	  NEXUS_SurfaceHandle fb1;
-	  NEXUS_SurfaceMemory mem;
-	  u32 pseudo_palette[16];
-	  unsigned ref_cnt;
+	NEXUS_Graphics2DHandle gfx;
+	struct bcmnexusfb_token hd;
+#if NEXUS_NUM_DISPLAYS > 1 && NEXUS_NUM_COMPOSITE_OUTPUTS
+	struct bcmnexusfb_token sd;
+#endif
+	u32 pseudo_palette[16];
+	unsigned ref_cnt;
 };
 
 static struct fb_fix_screeninfo bcmnexusfb_fix __initdata = {
@@ -68,10 +78,10 @@ static int bcmnexusfb_open(struct fb_info *info, int user)
 {
 	struct bcmnexusfb_par *par = (struct bcmnexusfb_par*)info->par;
 	NEXUS_PlatformConfiguration platformConfig;
-	/* NEXUS_PlatformSettings platformSettings; */
 	NEXUS_DisplaySettings displaySettings;
 	NEXUS_SurfaceCreateSettings createSettings;
 	NEXUS_GraphicsSettings graphicsSettings;
+	NEXUS_Graphics2DSettings gfxSettings;
 	NEXUS_VideoFormatInfo videoFormatInfo;
 	NEXUS_HdmiOutputHandle hdmiOutput;
 	NEXUS_HdmiOutputStatus hdmiStatus;
@@ -106,12 +116,20 @@ static int bcmnexusfb_open(struct fb_info *info, int user)
 		printk(".");
 		msleep(250);
 	} while (timeout--);
-	par->display = NEXUS_Display_Open(0, &displaySettings);
-	if(!par->display) {goto err_display;}
-	rc = NEXUS_Display_AddOutput(par->display, NEXUS_HdmiOutput_GetVideoConnector(hdmiOutput));
-	if(rc!=NEXUS_SUCCESS) {goto err_output;}
+
+	if (timeout > 0) {
+		par->hd.display = NEXUS_Display_Open(0, &displaySettings);
+		if(!par->hd.display) goto err_display;
+		rc = NEXUS_Display_AddOutput(par->hd.display, NEXUS_HdmiOutput_GetVideoConnector(hdmiOutput));
+		if(rc!=NEXUS_SUCCESS) goto err_output;
+	} else
+		par->hd.display = NULL;
+
 	par->gfx = NEXUS_Graphics2D_Open(0, NULL);
-	if(!par->gfx) {goto err_graphics;}
+	if(!par->gfx) goto err_graphics;
+	NEXUS_Graphics2D_GetSettings(par->gfx, &gfxSettings);
+	gfxSettings.pollingCheckpoint = true;
+	NEXUS_Graphics2D_SetSettings(par->gfx, &gfxSettings);
 
 	NEXUS_VideoFormat_GetInfo(displaySettings.format, &videoFormatInfo);
 	NEXUS_Surface_GetDefaultCreateSettings(&createSettings);
@@ -120,49 +138,51 @@ static int bcmnexusfb_open(struct fb_info *info, int user)
 	createSettings.height      = videoFormatInfo.height;
 	createSettings.pitch       = videoFormatInfo.width * 2 * 4;
 	createSettings.heap        = NEXUS_Platform_GetFramebufferHeap(0);
-	par->framebuffer           = NEXUS_Surface_Create(&createSettings);
-	if(par->framebuffer == NULL) {goto err_graphics;}
-	rc = NEXUS_Surface_GetMemory(par->framebuffer, &par->mem);
-	if(rc!=NEXUS_SUCCESS) {goto err_surface;}
+	par->hd.framebuffer        = NEXUS_Surface_Create(&createSettings);
+	if(par->hd.framebuffer == NULL) goto err_graphics;
+	rc = NEXUS_Surface_GetMemory(par->hd.framebuffer, &par->hd.mem);
+	if(rc!=NEXUS_SUCCESS) goto err_surface;
 
 	NEXUS_Surface_GetDefaultCreateSettings(&createSettings);
 	createSettings.pixelFormat = NEXUS_PixelFormat_eX8_B8_G8_R8;
 	createSettings.width       = videoFormatInfo.width;
 	createSettings.height      = videoFormatInfo.height;
 	createSettings.pitch       = videoFormatInfo.width * 4;
-	createSettings.pMemory     = par->mem.buffer;
-	par->fb0                   = NEXUS_Surface_Create(&createSettings);
-	if(par->fb0 == NULL) {goto err_surface;}
-	printk("fb%d-0::%dx%d::s:%d::b:%x::o:%p::%p\n", info->node,
+	createSettings.pMemory     = par->hd.mem.buffer;
+	par->hd.fb0                = NEXUS_Surface_Create(&createSettings);
+	if(par->hd.fb0 == NULL) goto err_surface;
+	printk("hdfb%d-0::%dx%d::s:%d::b:%p::o:%p::%p\n", info->node,
 		createSettings.width, createSettings.height,
 		createSettings.pitch, createSettings.pMemory,
-		createSettings.pMemory, par->fb0);
+		createSettings.pMemory, par->hd.fb0);
 
 	NEXUS_Surface_GetDefaultCreateSettings(&createSettings);
 	createSettings.pixelFormat = NEXUS_PixelFormat_eX8_B8_G8_R8;
 	createSettings.width       = videoFormatInfo.width;
 	createSettings.height      = videoFormatInfo.height;
 	createSettings.pitch       = videoFormatInfo.width * 4;
-	createSettings.pMemory     = par->mem.buffer + (videoFormatInfo.height * videoFormatInfo.width * 4);
-	par->fb1                   = NEXUS_Surface_Create(&createSettings);
-	if(par->fb1 == NULL) {goto err_surface;}
-	printk("fb%d-1::%dx%d::s:%d::b:%x::o:%p::%p\n", info->node,
+	createSettings.pMemory     = par->hd.mem.buffer + (videoFormatInfo.height * videoFormatInfo.width * 4);
+	par->hd.fb1                = NEXUS_Surface_Create(&createSettings);
+	if(par->hd.fb1 == NULL) goto err_surface;
+	printk("hdfb%d-1::%dx%d::s:%d::b:%p::o:%p::%p\n", info->node,
 		createSettings.width, createSettings.height,
 		createSettings.pitch, createSettings.pMemory,
-		createSettings.pMemory, par->fb1);
+		createSettings.pMemory, par->hd.fb1);
 
-	NEXUS_Display_GetGraphicsSettings(par->display, &graphicsSettings);
-	graphicsSettings.enabled           = true;
-	graphicsSettings.clip.width        = videoFormatInfo.width;
-	graphicsSettings.clip.height       = videoFormatInfo.height;
-	graphicsSettings.sourceBlendFactor = NEXUS_CompositorBlendFactor_eOne;
-	graphicsSettings.destBlendFactor   = NEXUS_CompositorBlendFactor_eZero;
-	graphicsSettings.constantAlpha     = 0xFF;
-	NEXUS_Display_SetGraphicsSettings(par->display, &graphicsSettings);
+	if (par->hd.display) {
+		NEXUS_Display_GetGraphicsSettings(par->hd.display, &graphicsSettings);
+		graphicsSettings.enabled           = true;
+		graphicsSettings.clip.width        = videoFormatInfo.width;
+		graphicsSettings.clip.height       = videoFormatInfo.height;
+		graphicsSettings.sourceBlendFactor = NEXUS_CompositorBlendFactor_eOne;
+		graphicsSettings.destBlendFactor   = NEXUS_CompositorBlendFactor_eZero;
+		graphicsSettings.constantAlpha     = 0xFF;
+		NEXUS_Display_SetGraphicsSettings(par->hd.display, &graphicsSettings);
+	}
 
-	info->fix.smem_start  = (unsigned long)NEXUS_AddrToOffset(par->mem.buffer);
-	info->fix.line_length = par->mem.pitch / 2;
-	info->fix.smem_len    = createSettings.height * par->mem.pitch;
+	info->fix.smem_start  = (unsigned long)NEXUS_AddrToOffset(par->hd.mem.buffer);
+	info->fix.line_length = par->hd.mem.pitch / 2;
+	info->fix.smem_len    = createSettings.height * par->hd.mem.pitch;
 	info->fix.visual      = FB_VISUAL_TRUECOLOR;
 
 	info->var.xres         = createSettings.width;
@@ -170,30 +190,105 @@ static int bcmnexusfb_open(struct fb_info *info, int user)
 	info->var.xres_virtual = createSettings.width;
 	info->var.yres_virtual = createSettings.height;
 
-	info->screen_base = (char __force __iomem *)par->mem.buffer;
+	info->screen_base = (char __force __iomem *)par->hd.mem.buffer;
 	info->screen_size = info->fix.smem_len / 2;
 
+#if NEXUS_NUM_DISPLAYS > 1 && NEXUS_NUM_COMPOSITE_OUTPUTS
+	rc = NEXUS_HdmiOutput_GetStatus(hdmiOutput, &hdmiStatus);
+	if (rc == NEXUS_SUCCESS && hdmiStatus.connected) {
+		par->sd.display = NULL;
+		par->sd.framebuffer = NULL;
+		goto done;
+	}
+
+	NEXUS_Display_GetDefaultSettings(&displaySettings);
+	displaySettings.format = NEXUS_VideoFormat_ePal;
+	par->sd.display = NEXUS_Display_Open(1, &displaySettings);
+	if(!par->sd.display) goto err_display;
+	rc = NEXUS_Display_AddOutput(par->sd.display, NEXUS_CompositeOutput_GetConnector(platformConfig.outputs.composite[0]));
+	if(rc!=NEXUS_SUCCESS) goto err_output;
+
+	NEXUS_VideoFormat_GetInfo(displaySettings.format, &videoFormatInfo);
+	NEXUS_Surface_GetDefaultCreateSettings(&createSettings);
+	createSettings.pixelFormat = NEXUS_PixelFormat_eX8_B8_G8_R8;
+	createSettings.width       = videoFormatInfo.width * 2;
+	createSettings.height      = videoFormatInfo.height;
+	createSettings.pitch       = videoFormatInfo.width * 2 * 4;
+	createSettings.heap        = NEXUS_Platform_GetFramebufferHeap(0);
+	par->sd.framebuffer = NEXUS_Surface_Create(&createSettings);
+	if(par->sd.framebuffer == NULL) goto err_graphics;
+	rc = NEXUS_Surface_GetMemory(par->sd.framebuffer, &(par->sd.mem));
+	if(rc!=NEXUS_SUCCESS) goto err_surface;
+
+	NEXUS_Surface_GetDefaultCreateSettings(&createSettings);
+	createSettings.pixelFormat = NEXUS_PixelFormat_eX8_B8_G8_R8;
+	createSettings.width       = videoFormatInfo.width;
+	createSettings.height      = videoFormatInfo.height;
+	createSettings.pitch       = videoFormatInfo.width * 4;
+	createSettings.pMemory     = par->sd.mem.buffer;
+	par->sd.fb0 = NEXUS_Surface_Create(&createSettings);
+	if(par->sd.fb0 == NULL) goto err_surface;
+	printk("sdfb%d-0::%dx%d::s:%d::b:%p::o:%p::%p\n", info->node,
+		createSettings.width, createSettings.height,
+		createSettings.pitch, createSettings.pMemory,
+		createSettings.pMemory, par->sd.fb0);
+
+	NEXUS_Surface_GetDefaultCreateSettings(&createSettings);
+	createSettings.pixelFormat = NEXUS_PixelFormat_eX8_B8_G8_R8;
+	createSettings.width       = videoFormatInfo.width;
+	createSettings.height      = videoFormatInfo.height;
+	createSettings.pitch       = videoFormatInfo.width * 4;
+	createSettings.pMemory     = par->sd.mem.buffer + (videoFormatInfo.height * videoFormatInfo.width * 4);
+	par->sd.fb1 = NEXUS_Surface_Create(&createSettings);
+	if(par->sd.fb1 == NULL) goto err_surface;
+	printk("sdfb%d-1::%dx%d::s:%d::b:%p::o:%p::%p\n", info->node,
+		createSettings.width, createSettings.height,
+		createSettings.pitch, createSettings.pMemory,
+		createSettings.pMemory, par->sd.fb1);
+
+	NEXUS_Display_GetGraphicsSettings(par->sd.display, &graphicsSettings);
+	graphicsSettings.enabled           = true;
+	graphicsSettings.clip.width        = videoFormatInfo.width;
+	graphicsSettings.clip.height       = videoFormatInfo.height;
+	graphicsSettings.sourceBlendFactor = NEXUS_CompositorBlendFactor_eOne;
+	graphicsSettings.destBlendFactor   = NEXUS_CompositorBlendFactor_eZero;
+	graphicsSettings.constantAlpha     = 0xFF;
+	NEXUS_Display_SetGraphicsSettings(par->sd.display, &graphicsSettings);
+#endif
+
 done:
-	printk("fb%d::%dx%d::v:%dx%d::base:%x::sz:%u::%u:%u/%zu\n",
+	printk("fb%d::%dx%d::v:%dx%d::base:%p::sz:%lu::%u:%u/%zu\n",
 		info->node,
 		info->var.xres, info->var.yres,
 		info->var.xres_virtual, info->var.yres_virtual,
-		info->screen_base, info->screen_size,
-		info->fix.line_length, info->fix.smem_len, par->mem.bufferSize);
+		(void *)info->screen_base, info->screen_size,
+		info->fix.line_length, info->fix.smem_len,
+		par->hd.mem.bufferSize);
 	par->ref_cnt++;
 	return 0;
 
 err_surface:
 	printk("fb%d: err_surface - FAILED\n", info->node);
-	if (par->fb0) NEXUS_Surface_Destroy(par->fb0);
-	if (par->fb1) NEXUS_Surface_Destroy(par->fb1);
-	if (par->framebuffer) NEXUS_Surface_Destroy(par->framebuffer);
+	if (par->hd.fb0) NEXUS_Surface_Destroy(par->hd.fb0);
+	if (par->hd.fb1) NEXUS_Surface_Destroy(par->hd.fb1);
+	if (par->hd.framebuffer) NEXUS_Surface_Destroy(par->hd.framebuffer);
+#if NEXUS_NUM_DISPLAYS > 1 && NEXUS_NUM_COMPOSITE_OUTPUTS
+	if (par->sd.fb0) NEXUS_Surface_Destroy(par->sd.fb0);
+	if (par->sd.fb1) NEXUS_Surface_Destroy(par->sd.fb1);
+	if (par->sd.framebuffer) NEXUS_Surface_Destroy(par->sd.framebuffer);
+#endif
 err_graphics:
 	printk("fb%d: err_graphics - FAILED\n", info->node);
-	NEXUS_Display_RemoveAllOutputs(par->display);
+	if (par->hd.display) NEXUS_Display_RemoveAllOutputs(par->hd.display);
+#if NEXUS_NUM_DISPLAYS > 1 && NEXUS_NUM_COMPOSITE_OUTPUTS
+	if (par->sd.display) NEXUS_Display_RemoveAllOutputs(par->sd.display);
+#endif
 err_output:
 	printk("fb%d: err_output - FAILED\n", info->node);
-	NEXUS_Display_Close(par->display);
+	if (par->hd.display) NEXUS_Display_Close(par->hd.display);
+#if NEXUS_NUM_DISPLAYS > 1 && NEXUS_NUM_COMPOSITE_OUTPUTS
+	if (par->sd.display) NEXUS_Display_Close(par->sd.display);
+#endif
 err_display:
 	printk("fb%d: err_display - FAILED\n", info->node);
 	return -ENODEV;
@@ -215,11 +310,17 @@ static int bcmnexusfb_release(struct fb_info *info, int user)
 	}
 
 	/* NEXUS_Display_RemoveAllOutputs(par->display); - not needed. */
-	NEXUS_Display_Close(par->display);
 	NEXUS_Graphics2D_Close(par->gfx);
-	NEXUS_Surface_Destroy(par->fb0);
-	NEXUS_Surface_Destroy(par->fb1);
-	NEXUS_Surface_Destroy(par->framebuffer);
+	if (par->hd.display) NEXUS_Display_Close(par->hd.display);
+	if (par->hd.fb0) NEXUS_Surface_Destroy(par->hd.fb0);
+	if (par->hd.fb1) NEXUS_Surface_Destroy(par->hd.fb1);
+	if (par->hd.framebuffer) NEXUS_Surface_Destroy(par->hd.framebuffer);
+#if NEXUS_NUM_DISPLAYS > 1 && NEXUS_NUM_COMPOSITE_OUTPUTS
+	if (par->sd.display) NEXUS_Display_Close(par->sd.display);
+	if (par->sd.fb0) NEXUS_Surface_Destroy(par->sd.fb0);
+	if (par->sd.fb1) NEXUS_Surface_Destroy(par->sd.fb1);
+	if (par->sd.framebuffer) NEXUS_Surface_Destroy(par->sd.framebuffer);
+#endif
 	return 0;
 }
 
@@ -259,10 +360,18 @@ static int bcmnexusfb_blank(int blank_mode, struct fb_info *info)
 
 	printk("fb%d: blank %d\n", info->node, blank_mode);
 
-	NEXUS_Display_GetGraphicsSettings(par->display, &graphicsSettings);
-	graphicsSettings.visible = blank_mode==FB_BLANK_UNBLANK?true:false;
-	NEXUS_Display_SetGraphicsSettings(par->display, &graphicsSettings);
-
+	if (par->hd.display) {
+		NEXUS_Display_GetGraphicsSettings(par->hd.display, &graphicsSettings);
+		graphicsSettings.visible = blank_mode==FB_BLANK_UNBLANK?true:false;
+		NEXUS_Display_SetGraphicsSettings(par->hd.display, &graphicsSettings);
+	}
+#if NEXUS_NUM_DISPLAYS > 1 && NEXUS_NUM_COMPOSITE_OUTPUTS
+	if (par->sd.display) {
+		NEXUS_Display_GetGraphicsSettings(par->sd.display, &graphicsSettings);
+		graphicsSettings.visible = blank_mode==FB_BLANK_UNBLANK?true:false;
+		NEXUS_Display_SetGraphicsSettings(par->sd.display, &graphicsSettings);
+	}
+#endif
 	return 0;
 }
 
@@ -272,7 +381,7 @@ void bcmnexusfb_fillrect(struct fb_info *p, const struct fb_fillrect *region)
 	NEXUS_Graphics2DFillSettings fillSettings;
 
 	NEXUS_Graphics2D_GetDefaultFillSettings(&fillSettings);
-	fillSettings.surface = par->framebuffer;
+	fillSettings.surface = par->hd.framebuffer;
 	fillSettings.rect.x = region->dx;
 	fillSettings.rect.y = region->dy;
 	fillSettings.rect.width = region->width;
@@ -292,13 +401,13 @@ void bcmnexusfb_copyarea(struct fb_info *p, const struct fb_copyarea *area)
 
 	NEXUS_Graphics2D_GetDefaultBlitSettings(&blitSettings);
 
-	blitSettings.source.surface = par->framebuffer;
+	blitSettings.source.surface = par->hd.framebuffer;
 	blitSettings.source.rect.x = area->sx;
 	blitSettings.source.rect.y = area->sy;
 	blitSettings.source.rect.width = area->width;
 	blitSettings.source.rect.height = area->height;
 
-	blitSettings.output.surface = par->framebuffer;
+	blitSettings.output.surface = par->hd.framebuffer;
 	blitSettings.output.rect.x = area->dx;
 	blitSettings.output.rect.y = area->dy;
 	blitSettings.output.rect.width = area->width;
@@ -324,7 +433,10 @@ int bcmnexusfb_sync(struct fb_info *info)
 
 	if (debug_verbose) printk("fb%d: sync\n", info->node);
 
-	NEXUS_Surface_Flush(par->framebuffer);
+	NEXUS_Surface_Flush(par->hd.framebuffer);
+#if NEXUS_NUM_DISPLAYS > 1 && NEXUS_NUM_COMPOSITE_OUTPUTS
+	if (par->sd.framebuffer) NEXUS_Surface_Flush(par->sd.framebuffer);
+#endif
 	return 0;
 }
 
@@ -343,30 +455,73 @@ int bcmnexusfb_set_par(struct fb_info *info)
 {
 	struct bcmnexusfb_par *par = (struct bcmnexusfb_par*)info->par;
 
-	if (info->flags & FBINFO_MISC_USEREVENT) {
-		if (debug_verbose) printk("fb%d: set-par:%d >> set-fb:%p\n",
-			info->node, info->var.yoffset,
-			!info->var.yoffset ? par->fb0 : par->fb1);
+	if (!(info->flags & FBINFO_MISC_USEREVENT)) return 0;
+
+	if (debug_verbose) printk("fb%d: set-par:%d >> set-fb:%p\n",
+		info->node, info->var.yoffset,
+		!info->var.yoffset ? par->hd.fb0 : par->hd.fb1);
+
+	if (par->hd.display) {
+		NEXUS_Display_SetGraphicsFramebuffer(
+			par->hd.display,
+			!info->var.yoffset ? par->hd.fb0 : par->hd.fb1);
+	}
+
+#if NEXUS_NUM_DISPLAYS > 1 && NEXUS_NUM_COMPOSITE_OUTPUTS
+	if (par->sd.display) {
+		NEXUS_Error rc;
+		int timeout = 10;
+
+		NEXUS_GraphicsSettings graphicsSettings;
+		NEXUS_Graphics2DBlitSettings blitSettings;
+		NEXUS_Graphics2D_GetDefaultBlitSettings(&blitSettings);
+		if (!info->var.yoffset) {
+			blitSettings.source.surface = par->hd.fb0;
+			blitSettings.output.surface = par->sd.fb0;
+		} else {
+			blitSettings.source.surface = par->hd.fb1;
+			blitSettings.output.surface = par->sd.fb1;
+		}
+		if (par->hd.display) {
+			NEXUS_Display_GetGraphicsSettings(par->hd.display, &graphicsSettings);
+			blitSettings.source.rect.width = graphicsSettings.clip.width;
+			blitSettings.source.rect.height = graphicsSettings.clip.height;
+		} else {
+			blitSettings.source.rect.width = 1920;
+			blitSettings.source.rect.height = 1080;
+		}
+		NEXUS_Display_GetGraphicsSettings(par->sd.display, &graphicsSettings);
+		blitSettings.output.rect.width = graphicsSettings.clip.width;
+		blitSettings.output.rect.height = graphicsSettings.clip.height;
+		blitSettings.colorOp = NEXUS_BlitColorOp_eCopySource;
+		blitSettings.alphaOp = NEXUS_BlitAlphaOp_eCopySource;
+		rc = NEXUS_Graphics2D_Blit(par->gfx, &blitSettings);
+		do {
+			rc = NEXUS_Graphics2D_Checkpoint(par->gfx, NULL);
+			if (rc != NEXUS_GRAPHICS2D_BUSY) break;
+			msleep(100);
+		} while (timeout--);
 
 		NEXUS_Display_SetGraphicsFramebuffer(
-			par->display,
-			!info->var.yoffset ? par->fb0 : par->fb1);
+			par->sd.display,
+			!info->var.yoffset ? par->sd.fb0: par->sd.fb1);
 	}
+#endif
 
 	return 0;
 }
 
 static struct fb_ops bcmnexusfb_ops = {
 	.owner			= THIS_MODULE,
-	.fb_open			= bcmnexusfb_open,
+	.fb_open		= bcmnexusfb_open,
 	.fb_release		= bcmnexusfb_release,
-	.fb_setcolreg	= bcmnexusfb_setcolreg,
+	.fb_setcolreg		= bcmnexusfb_setcolreg,
 	.fb_blank		= bcmnexusfb_blank,
-	.fb_fillrect	= bcmnexusfb_fillrect,
-	.fb_copyarea	= bcmnexusfb_copyarea,
-	.fb_imageblit	= bcmnexusfb_imageblit,
-	.fb_sync			= bcmnexusfb_sync,
-	.fb_check_var	= bcmnexusfb_check_var,
+	.fb_fillrect		= bcmnexusfb_fillrect,
+	.fb_copyarea		= bcmnexusfb_copyarea,
+	.fb_imageblit		= bcmnexusfb_imageblit,
+	.fb_sync		= bcmnexusfb_sync,
+	.fb_check_var		= bcmnexusfb_check_var,
 	.fb_set_par		= bcmnexusfb_set_par,
 };
 
@@ -425,7 +580,7 @@ static void __exit bcmnexusfb_exit(void)
 		bcmfb = NULL;
 	}
 
-	return 0;
+	return;
 }
 
 module_init(bcmnexusfb_init);
